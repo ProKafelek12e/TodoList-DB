981daa94cb808b6e9a58672d93e35bac07c33e99
Karol Kędziora <pro.kafelek@gmail.com>
2023-03-03 16:05:08 +0100

    End

---
+++ client/app.js
@@ -0,0 +1,58 @@
+var json = []
+async function getData(){
+    const data = await fetch(`http://localhost:3000/gettask`)
+    json = await data.json()
+    await createTasks()
+}
+getData()
+
+async function addtask(){
+    const nazwa = document.getElementById("inpNazwa").value
+    const termin = document.getElementById("inpTermin").value
+    document.getElementById("inpNazwa").value = ""
+    document.getElementById("inpTermin").value = ""
+    const add = await fetch(`${baseurl}/addtask/${nazwa}/${termin}`)
+    getData()
+}
+
+function createTasks(){
+    document.getElementById("main").innerHTML = ""
+    for(var i = 0;i<=json.length-1;i++){
+        const div = document.createElement("div")
+        div.classList.add("div")
+
+
+        const termin = document.createElement("h1")
+        termin.innerHTML = json[i].termin
+        termin.classList.add("termin")
+        
+        const tresc = document.createElement("h1")
+        tresc.innerHTML = json[i].nazwa
+        const guzi = document.createElement("div")
+        guzi.classList.add("guzi")
+        const button =document.createElement("button")
+        button.innerHTML = "🗑️"
+        button.setAttribute("onclick",`deltask(${json[i].ID})`)
+        
+        const done = document.createElement("button")
+        if(json[i].done==0) done.innerHTML = "❌"
+        else done.innerHTML = "✔️"
+        
+        done.classList.add("undone")
+        done.setAttribute("onclick",`done(${json[i].ID},${json[i].done})`)
+        div.appendChild(tresc)
+        div.appendChild(termin)
+        div.appendChild(guzi)
+        guzi.appendChild(done)
+        guzi.appendChild(button)
+        document.getElementById("main").appendChild(div)
+    }
+}
+async function deltask(id){
+    await fetch(`${baseurl}/deltask/${id}`)
+    getData()
+}
+async function done(id,done){
+    await fetch(`${baseurl}/done/${id}/${done}`)
+    getData()
+}
--- server/index.js
+++ server/index.js
@@ -46,7 +46,27 @@ app.get("/deltask/:ID",function(req,res){
         console.log(fields)
     })
 })
-    
+
+app.get("/done/:ID/:done",function(req,res){
+    const ID = req.params.ID
+    const done = req.params.done
+    if(done==0){
+        const sql = `UPDATE base SET done = '1' WHERE ID = ${ID};`
+        con.query(sql,function(err,result,fields){
+            if(err) console.log("error: "+err)
+            res.send("zmienion")
+            console.log(fields)
+        })
+    }  
+    else if(done==1){
+        const sql = `UPDATE base SET done = '0' WHERE ID = ${ID};`
+        con.query(sql,function(err,result,fields){
+            if(err) console.log("error: "+err)
+            res.send("zmienion")
+            console.log(fields)
+        })
+    }    
+})
 
 
 app.listen(3000)
